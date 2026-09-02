6c52b93e1d6f6f878b3538d75b29b6e0a67296a3
Bready <joshbreadmore@gmail.com>
2026-09-02 16:47:29 +1200

    add lb ( cant run on free tier

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
+data "aws_availability_zones" "all" {}
+
 resource "aws_launch_configuration" "example" {
   image_id = "ami-06259b63260eddc13"
   instance_type = "t3.micro"
@@ -35,7 +37,10 @@ resource "aws_security_group" "instance" {
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = "${aws_launch_configuration.example.id}"
-  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  availability_zones = data.aws_availability_zones.all.names
+
+  load_balancers = [aws_elb.example.name]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
@@ -47,12 +52,59 @@ resource "aws_autoscaling_group" "example" {
   }
 }
 
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  availability_zones = data.aws_availability_zones.all.names
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  listener {
+    lb_port = "${var.elb_port}"
+    lb_protocol = "http"
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name = "tf-sg-elb"
+  
+  ingress {
+    from_port = "${var.elb_port}"
+    to_port = "${var.elb_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
 variable "server_port" {
   description = "default 8080 port"
   default = 8080
 }
 
-output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
+variable "elb_port" {
+  description = "default 8080 port"
+  default = 80
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
 }
