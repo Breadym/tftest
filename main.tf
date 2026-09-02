8dbb41982bca16e44cd777969d8c665653faf4f3
Bready <joshbreadmore@gmail.com>
2026-09-02 16:17:21 +1200

    add aws_autoscaling_group

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-06259b63260eddc13"
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-06259b63260eddc13"
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  security_groups = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -13,9 +13,8 @@ resource "aws_instance" "example" {
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
   
-  tags = {
-    Name = "New-VM"
-
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -28,7 +27,26 @@ resource "aws_security_group" "instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
 
 variable "server_port" {
   description = "default 8080 port"
