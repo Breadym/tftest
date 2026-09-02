7f3dba73b00d17dc3ce864e0da65cb9bd7479e85
Bready <joshbreadmore@gmail.com>
2026-09-02 15:24:36 +1200

    add hello world

--- main.tf
+++ main.tf
@@ -5,9 +5,27 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-06259b63260eddc13"
   instance_type = "t3.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
   
   tags = {
     Name = "my-vm-git"
 
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "tf-example-instance"
+  
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
